f7801c3526c3ffc40c3550700e7987008dbc7b2a
Stuart White <srm.white@yahoo.co.uk>
2018-08-28 20:09:19 +0100

    Functions started

---
+++ Functions/facts.js
@@ -0,0 +1,6 @@
+function parrotFacts() {
+  console.log('Some parrot species can live for over 80 years');
+  console.log('Kakapos are a critically endangered flightless parrot');
+}
+
+parrotFacts()
