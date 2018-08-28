2306e6647a618a33b8115d4de3b411b865991d5d
Stuart White <srm.white@yahoo.co.uk>
2018-08-28 21:26:42 +0100

    Arguments in functions

--- Functions/facts.js
+++ Functions/facts.js
@@ -1,6 +1,27 @@
+// parrotFacts function created
 function parrotFacts() {
   console.log('Some parrot species can live for over 80 years');
   console.log('Kakapos are a critically endangered flightless parrot');
 }
 
+// Call parrotFacts function
 parrotFacts()
+
+// Arguments - Functions can accept input values called arguments.
+
+function callKitten(kittenName) {
+  console.log('Come here, ' + kittenName + '!');
+}
+
+// Call the function callKitten and add the argument 'Fluffy'. An argument seems to be similar to a variable, maybe you could use a variable as an argument?
+
+callKitten('Fluffy'); // Outputs 'Come here, Fluffy!'
+
+// Number function
+
+function addNumbers(a, b) {
+  console.log(a + b);
+}
+
+addNumbers(5, 7); // Outputs the value 12
+addNumbers(9, 12); // Outputs the value 21
