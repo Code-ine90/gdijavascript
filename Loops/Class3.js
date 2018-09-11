50b73b3fd60d685486efea40c2d3682e4cea51e1
Stuart White <srm.white@yahoo.co.uk>
2018-09-11 19:33:06 +0100

    new loops

--- Loops/Class3.js
+++ Loops/Class3.js
@@ -13,3 +13,32 @@ while (bottlesOfBeer > 0) {
 for (var i = 1; i <= 10; i++) {
   console.log(i);
 }
+
+// you can add other statements or logical operators inside loops
+
+for (var i2 = 1; i2 <= 100; i2++) {
+  if (i2 % 3 === 0) {
+    // Says fizz after multiples of three
+    console.log(i2 + ' fizz');
+  }
+  else if (i2 % 5 === 0) {
+    // Says buzz after multiples of five
+    console.log(i2 + ' buzz');
+  }
+  else {
+    console.log(i2);
+  }
+}
+
+// to exit a loop you use the break statement
+
+// count from 100 to 200
+for (var i3 = 100; i3 <= 200; i3++) {
+  console.log('Testing ' + i3);
+
+  //stop at the first multiple of 7
+  if (i3 % 7 === 0) {
+    console.log('Found it! ' + i3);
+    break;
+  }
+}
