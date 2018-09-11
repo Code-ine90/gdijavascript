8f18456a13b18101935b4effd065ec731b3d6256
Stuart White <srm.white@yahoo.co.uk>
2018-09-11 19:19:55 +0100

    loops started

---
+++ Loops/Class3.js
@@ -0,0 +1,15 @@
+var bottlesOfBeer = 99;
+
+while (bottlesOfBeer > 0) {
+  console.log(bottlesOfBeer + ' bottles of beer on the wall!');
+  bottlesOfBeer = bottlesOfBeer - 1;
+}
+
+// while loops will repeat the same code over and over until a condition is met. In this case, the variable for bottlesOfBeer drops to 0.
+
+
+// for loops are very similar, but you declare a counter in the statement.
+
+for (var i = 1; i <= 10; i++) {
+  console.log(i);
+}
