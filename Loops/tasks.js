e7ba68200f396c30df68e7defc9c8c74326a56c3
Stuart White <srm.white@yahoo.co.uk>
2018-09-12 19:13:12 +0100

    task started

---
+++ Loops/tasks.js
@@ -0,0 +1,5 @@
+// Write a loop that gives you the 9's times table,
+// from 9 x 1 = 9 to 9 x 12 = 108.
+//
+//
+// Finish early? Try using a loop inside a loop to write all the times tables, from 1 to 12.
