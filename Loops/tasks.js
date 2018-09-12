247d3b4dc92a2e46480a319e656ba4d615eb2c6c
Stuart White <srm.white@yahoo.co.uk>
2018-09-12 19:22:48 +0100

    task one complete

--- Loops/tasks.js
+++ Loops/tasks.js
@@ -3,3 +3,12 @@
 //
 //
 // Finish early? Try using a loop inside a loop to write all the times tables, from 1 to 12.
+console.log ('The tasks start here...')
+
+var count
+
+for (count = 1; count <= 108; count++) {
+  if (count % timeTable === 0) {
+    console.log(count);
+  }
+}
