a50aec9a4911c18209a7790bab79b1260b11e051
Stuart White <srm.white@yahoo.co.uk>
2018-09-14 12:31:37 +0100

    added test page for styling

--- Class 4/Class4.js
+++ Class 4/Class4.js
@@ -40,3 +40,40 @@ for (var i = 0; i < listItems.length; i++) {
   var listItem = listItems[i];
   console.log(listItem);
 }
+
+// DOM Access: HTML 5
+// In newer browsers, you can use methods getElementsByClassName, querySelector, and querySelectorAll.
+//
+//
+// Available in IE9+, FF3.6+, Chrome 17+, Safari 5+:
+//
+// document.getElementsByClassName(className);
+//
+// Available in IE8+, FF3.6+, Chrome 17+, Safari 5+:
+//
+// document.querySelector(cssQuery);
+// document.querySelectorAll(cssQuery);
+
+// getElement vs. getElements
+// Any method that starts with getElement will return a single node.
+//
+// document.getElementById('uniqueID'); // returns a single node
+//
+// Any method that starts with getElements will return an array of nodes. To modify a single node, you will need to use bracket notation to get the correct one.
+//
+// document.getElementsByTagName('p'); // returns multiple nodes
+//
+// var specificParagraph = document.getElementsByTagName('p')[2];
+
+// Changing nodes
+
+// You can change the page CSS using Style
+
+// To make this CSS:
+//
+// body {
+//   color: red;
+// }
+
+ var pageBody = document.getElementsByTagName('body') [0];
+ pageBody.style.color = 'red';
